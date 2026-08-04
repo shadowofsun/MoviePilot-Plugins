73464bb8c14e2dad8c1b2dec640e3f7b555db4ec
shadowofsun <zhubinquan86@gmail.com>
2026-08-04 22:51:05 +0800

    Publish Metadata115Sync v1.0.0

---
+++ plugins.v2/metadata115sync/src/components/Page.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import AppPage from './AppPage.vue'
+
+defineProps({
+  api: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+const emit = defineEmits(['close'])
+
+const pageRef = ref(null)
+</script>
+
+<template>
+  <div class="metadata115sync-page-wrapper">
+    <VToolbar density="comfortable" class="sticky-toolbar">
+      <div class="text-h6 ms-3">元数据115同步</div>
+      <VSpacer />
+      <VBtn icon="mdi-refresh" variant="text" :loading="pageRef?.loading" @click="pageRef?.loadStatus()" />
+      <VBtn icon="mdi-close" variant="text" @click="emit('close')" />
+    </VToolbar>
+    <VDivider />
+
+    <AppPage ref="pageRef" :api="api" plugin-id="Metadata115Sync" hide-title />
+  </div>
+</template>
+
+<style scoped>
+.sticky-toolbar {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background: rgb(var(--v-theme-surface));
+}
+</style>
