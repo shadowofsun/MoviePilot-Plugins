e4ca66fb2bf6c3310933c80d3faf2cd9b0f556df
shadowofsun <zhubinquan86@gmail.com>
2026-08-04 23:50:22 +0800

    Update Metadata115Sync v1.0.1

--- plugins.v2/metadata115sync/dist/assets/remoteEntry.js
+++ plugins.v2/metadata115sync/dist/assets/remoteEntry.js
@@ -2,14 +2,14 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./Page":()=>{
-      dynamicLoadingCss(["__federation_expose_Page-CGYXUrBX.css"], false, './Page');
-      return __federation_import('./__federation_expose_Page-j7qV3dom.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
+      dynamicLoadingCss(["__federation_expose_Page-9Jq-_JLN.css"], false, './Page');
+      return __federation_import('./__federation_expose_Page-63u4D1qY.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./Config":()=>{
       dynamicLoadingCss([], false, './Config');
-      return __federation_import('./__federation_expose_Config-DUoFNnOA.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
+      return __federation_import('./__federation_expose_Config-AP1jFd6l.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./AppPage":()=>{
       dynamicLoadingCss([], false, './AppPage');
-      return __federation_import('./__federation_expose_AppPage-C0INesKN.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      return __federation_import('./__federation_expose_AppPage-D37V3-FV.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
